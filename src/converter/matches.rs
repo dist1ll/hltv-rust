fbda658806e6c463f76c0fb155859c447d9410ed
Adrian <contact@alic.dev>
2022-01-30 18:16:08 +0100

    Simplify team parsing for upcoming matches

--- src/converter/matches.rs
+++ src/converter/matches.rs
@@ -9,7 +9,6 @@ impl<'a> ConvertCollection<'a> for UpcomingMatch {
         let match_containers = d.query_selector("div.upcomingMatch").unwrap();
         for c in match_containers {
             let stars = parse_stars(&d, c)?;
-            
         }
         Ok(result)
     }
@@ -17,28 +16,15 @@ impl<'a> ConvertCollection<'a> for UpcomingMatch {
 
 /// Returns a Team contained in the NodeHandle. Use tag `"team1"` or `"team2"`
 /// to search for either.
-fn parse_team(d: &tl::VDom, h: tl::NodeHandle, team_id: &str) -> Result<Option<Team>, Error> {
+fn parse_team(d: &tl::VDom, h: tl::NodeHandle, team_id: &str) -> Option<Team> {
     let tag = h.get(d.parser()).unwrap().as_tag().unwrap();
-    let id = match tag.get_attr(team_id)? {
-        Some(id) => id,
-        None => return Err(Error::ConversionError("no id attribute in div.upcomingMatch".to_string())),
-    };
-
-    let nodes = d.select_nodes(h, team_id);
-    if nodes.is_empty() {
-        return Ok(None);
-    }
-    let nodes = d.select_nodes(nodes[0], "matchTeamName");
-    if nodes.is_empty() {
-        return Ok(None);
-    }
-    match nodes[0].inner_text(d.parser()) {
-        Some(name) => Ok(Some(Team {
-            id,
-            name: name.to_string(),
-        })),
-        None => Ok(None),
-    }
+    let id: u32 = tag.get_attr(team_id).unwrap()?;
+    let team_node = d.select_first(h, team_id)?;
+    let name_node = d.select_first(team_node, "matchTeamName")?;
+    name_node.inner_text(d.parser()).map(|n| Team {
+        id,
+        name: n.to_string(),
+    })
 }
 /// Returns the number of stars in an upcoming match. Returns errors if
 /// the stars information is missing or the attribute cannot be parsed.
@@ -46,8 +32,10 @@ fn parse_stars(d: &tl::VDom, h: tl::NodeHandle) -> Result<u32, Error> {
     let tag = h.get(d.parser()).unwrap().as_tag().unwrap();
     match tag.get_attr("stars")? {
         Some(x) => Ok(x),
-        None => Err(Error::ConversionError("no stars attribute in div.upcomingMatch".to_string())),
-   }
+        None => Err(Error::ConversionError(
+            "no stars attribute in div.upcomingMatch".to_string(),
+        )),
+    }
 }
 
 #[cfg(test)]
